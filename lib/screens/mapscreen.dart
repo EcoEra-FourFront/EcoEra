62e834910a948938ea7526f439f032904421cc0a
Sharaneshwar Bharat Punjal <punjalsharaneshwar@gmail.com>
2024-02-10 15:33:54 +0530

    maps current location enabled

--- lib/screens/mapscreen.dart
+++ lib/screens/mapscreen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,17 +12,84 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  static const LatLng _pGooglePlex = LatLng(37.4223, -122.8848);
+  final Location _locationController = Location();
+
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+
+  LatLng? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: _pGooglePlex,
-          zoom: 13,
-        ),
-      ),
+    return Scaffold(
+      body: _currentPosition == null
+          ? const Center(
+              child: Text("Loading..."),
+            )
+          : GoogleMap(
+              onMapCreated: ((GoogleMapController controller) =>
+                  _mapController.complete(controller)),
+              initialCameraPosition: CameraPosition(
+                target: _currentPosition!,
+                zoom: 18,
+              ),
+              markers: {
+                Marker(
+                  markerId: MarkerId("current"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _currentPosition!,
+                ),
+              },
+            ),
     );
+  }
+
+  Future<void> _cameraToPosition(LatLng pos) async {
+    final GoogleMapController controller = await _mapController.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: pos,
+        zoom: 18,
+      ),
+    ));
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentPosition =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+          _cameraToPosition(_currentPosition!);
+        });
+      }
+    });
   }
 }
